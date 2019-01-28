6b20a0f0f68186218e3fa777323f266fcf16b5d4
Mathew Hall <mhall.js@gmail.com>
2019-01-28 16:45:40 +0000

    SASS added  to replace CSS

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers/routeReducer';
-import './App.css';
+import './App.scss';
 import TestComponent from './components/testComponent';
 
 class App extends Component {
